551015536c37b22535b28c63f8f373e2d977ab55
jaboulos <jab12288@gmail.com>
2021-02-05 18:59:54 -0800

    Add firebase subscription for user session persistence

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+// tool for saving state of a user that has signed in
+import { auth } from './firebase/firebase.utils';
 
-import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/Shop/ShopPage';
 import SignInAndSignUp from './pages/SignInAndSignUp/SignInAndSignUp';
 import Header from './components/Header/Header';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
-};
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  // subscriber to firebase, subscribes to the state of a user that has signed in or out
+  componentDidMount() {
+    // user parameter is what the user state is of the auth
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      // set currentUser to the user object
+      this.setState({ currentUser: user });
+      // console.log('user', user);
+    });
+  }
+
+  // close subscription when component unmounts
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default App;
