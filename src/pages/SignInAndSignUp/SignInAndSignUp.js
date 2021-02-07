944b94ba8f41ba6dafc13e741e01e885b694c98e
jaboulos <jab12288@gmail.com>
2021-02-07 15:50:27 -0800

    Include ability to store authenticated users into firebase db

--- src/pages/SignInAndSignUp/SignInAndSignUp.js
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
 
 import './signInAndSignUp.scss';
 
 const SignInAndSignUp = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
