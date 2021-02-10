a18a55d66c63bddbbfe7158870ec503835cbda19
jaboulos <jab12288@gmail.com>
2021-02-10 14:11:31 -0800

    Add mapDispatchToProps and connect to redux store

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Button from '../Button/Button';
+import { addItem } from '../../redux/cart/cartActions';
 
 import './collectionItem.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
-    </div>
-    <Button inverted>ADD TO CART</Button>
-  </div>
-);
+// addItem dispatch is now available to be passed in as props to CollectionItem
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
 
-export default CollectionItem;
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button onClick={() => addItem(item)} inverted>
+        ADD TO CART
+      </Button>
+    </div>
+  );
+};
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
