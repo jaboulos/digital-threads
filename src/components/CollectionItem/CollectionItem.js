eae55362825fe7ba2d6887c833f8fee3af62f877
jaboulos <jab12288@gmail.com>
2021-02-09 19:04:59 -0800

    Add Button component to CollectionItem component

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Button from '../Button/Button';
+
 import './collectionItem.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -14,6 +16,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
+    <Button inverted>ADD TO CART</Button>
   </div>
 );
 
