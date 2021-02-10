44ea72ee9013ad7d2bfee82aadd64281fefbf2f5
jaboulos <jab12288@gmail.com>
2021-02-10 14:12:35 -0800

    Remove destructure and pass item prop to CollectionItem

--- src/components/CollectionPreview/CollectionPreview.js
+++ src/components/CollectionPreview/CollectionPreview.js
@@ -11,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
       {/* only display 4 items at a time, use filter */}
       {items
         .filter((_, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
