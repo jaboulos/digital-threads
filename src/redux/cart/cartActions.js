736605b799c9c1b5f29687d539b26721d1acfbf5
jaboulos <jab12288@gmail.com>
2021-02-11 15:52:31 -0800

    Add decreaseItem action

--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -9,3 +9,12 @@ export const addItem = (item) => ({
   type: cartActionTypes.ADD_ITEM,
   payload: item,
 });
+export const decreaseItem = (item) => ({
+  type: cartActionTypes.DECREASE_ITEM,
+  payload: item,
+});
+
+export const clearItemFromCart = (item) => ({
+  type: cartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
