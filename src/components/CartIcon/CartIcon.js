c0447d1f9af9e01da5694d796e56d2bb56f884bb
jaboulos <jab12288@gmail.com>
2021-02-10 19:41:35 -0800

    Add selectors for hidden and itemsCount

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cartActions';
-import { selectCartItemsCount } from '../../selectors/cart/cartSelectors';
+import { selectCartItemsCount } from '../../selectors/cartSelectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
