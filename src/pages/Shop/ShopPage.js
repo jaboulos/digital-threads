3217ed1ae9c7b854047bd3bfce4eda4bd47ae196
jaboulos <jab12288@gmail.com>
2021-02-12 09:40:29 -0800

    Move CollectionPreview into CollectionsOverview component

--- src/pages/Shop/ShopPage.js
+++ src/pages/Shop/ShopPage.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-import { selectCollections } from '../../selectors/shopSelectors';
-
-import CollectionPreview from '../../components/CollectionPreview/CollectionPreview';
-
-const mapStateToProps = (state) => ({
-  collections: selectCollections(state),
-});
+import CollectionsOverview from '../../components/CollectionsOverview/CollectionsOverview';
 
 const ShopPage = ({ collections }) => (
   <div className='shop-page'>
-    {/* destructure properties from shop data and pass them all into CollectionPreview */}
-    {collections.map(({ id, ...rest }) => (
-      <CollectionPreview key={id} {...rest} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
