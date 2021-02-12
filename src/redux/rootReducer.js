c9b177184ea813bf2503bc099b94a50f3141c2cf
jaboulos <jab12288@gmail.com>
2021-02-11 19:34:32 -0800

    Add shopReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -5,6 +5,8 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/userReducer';
 import cartReducer from './cart/cartReducer';
+import directoryReducer from './directory/directoryReducer';
+import shopReducer from './shop/shopReducer';
 
 const persistConfig = {
   key: 'root',
@@ -16,6 +18,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
 
 // modified version of rootReducer with persistance capabilities
